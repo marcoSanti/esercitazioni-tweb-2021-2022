e73cf2cc21103bc81a4da1d94ce2144079664cc3
Marco Edoardo Santimaria <marco.santimaria@edu.unito.it>
2021-10-25 15:15:07 +0200

    More bug fixes

--- Consegna 3/movie.php
+++ Consegna 3/movie.php
@@ -2,56 +2,45 @@
 
     /*
      * getting the movie name.
-     * if it is not set, the title is set no empty string
+     * if it is not set, the title is obtained by the glob function. To be more precise,
+     * I set the title to the name of the first folder in the root folder.
      */
-    if(!isset($_GET['film'])) {
-        $title = '';
-    }else{
-        $title = $_GET['film'];
-    }
+
+    $title = $_GET['film'] ??  array_filter(glob("*"), 'is_dir')[0];
+
 
 
 
     /*
-     * Here i check that the movie exists. if it does not ( string empy, null or simply the movie folder does not exists),
-     * i set the title to the name of the first folder in the root folder.
-     * to be more precise, i use glob()[0] to obtain the array with all the folder inside the root folder,and i set that value to the var $title
-     * */
-    if($title=='' || $title == null || !file_exists( './' .$title .'/')){
-        $title =  glob('./*/')[0]; //getting only folders leaving out files
-    }
-
-    /*
-     * here i create the path to the movie image and the movie description file
-     * */
+     * here I create the path to the movie image and the movie description file
+     */
     $moviePicPath = './' .$title . '/overview.png';
     $movieInfo = file('./' . $title . '/info.txt');
 
     /*
-     * Here i proceed to load all the information of the movie into a string indexed array.
-     * */
+     * Here I proceed to load all the information of the movie into a string indexed array.
+     */
 
     $overview = array();
-    $overviewInput = file( './' .$title . '/overview.txt');
 
-    foreach ($overviewInput as $tmp){
+    foreach (file( './' .$title . '/overview.txt') as $tmp){
         $tmp = explode(':', $tmp);
         $overview[$tmp[0]] = Array($tmp[0],$tmp[1]);
     }
 
     /*
-     * here i do some html preformatting for cast members but only if i have a starring section in the overview array
-     * */
+     * here I do some html preformatting for cast members but only if I have a starring section in the overview array
+     */
     if(isset($overview['STARRING']))
         $overview['STARRING'][1] = str_replace(",", '<br>', $overview['STARRING'][1]);
 
     /*
-     * here i get all the reviews field paths and i put them inside an array
-     * */
-    $reviewNames = glob( './' .$title . '/review*.txt');
+     * here I get all the reviews field paths, and I put them inside an array
+     */
+
     $review = Array();
-    foreach ($reviewNames as $reviewName) {
-        $review[] = file($reviewName);
+    foreach (glob( './' .$title . '/review*.txt') as $reviewName) {
+        $review[] = file($reviewName, FILE_IGNORE_NEW_LINES);
     }
 
 
@@ -67,7 +56,7 @@
 </head>
 <body>
 <div class="header_banner">
-	<img src="http://www.cs.washington.edu/education/courses/cse190m/11sp/homework/2/banner.png" id="bannerbg" alt="Rancid Tomatoes">
+	<img src="https://www.cs.washington.edu/education/courses/cse190m/11sp/homework/2/banner.png" id="bannerbg" alt="Rancid Tomatoes">
 </div>
 <h1 class="movie_title"><?= $movieInfo[0] . ' ( ' . $movieInfo[1]  . ')' ?></h1>
 <div class ="page_content">
@@ -79,12 +68,12 @@
 			<dl>
                 <?php
                 /*
-                 * here i print all the overview array content, printing first the heading and then the value
+                 * here I print all the overview array content, printing first the heading and then the value
                  * */
                     foreach($overview as $tmp => $item){
                         ?>
-                        <dt><?= $overview[$tmp][0] ?></dt>
-                        <dd><?= $overview[$tmp][1] ?></dd>
+                        <dt><?= $item[0] ?></dt>
+                        <dd><?= $item[1] ?></dd>
                         <?php
                     }
                 ?>
@@ -94,13 +83,13 @@
 	<div class="left">
 		<div class="rotten_title_header">
             <?php
-                if($movieInfo[2] >=60){ //only if movie rating is > 60 i print the fresh
+                if($movieInfo[2] >=60){ //only if movie rating is > 60 I print the fresh
                     ?>
-                    <img src="http://www.cs.washington.edu/education/courses/cse190m/11sp/homework/2/freshbig.png" alt="Rotten">
+                    <img src="https://www.cs.washington.edu/education/courses/cse190m/11sp/homework/2/freshbig.png" alt="Rotten">
                     <?php
                 }else{
                     ?>
-                    <img src="http://www.cs.washington.edu/education/courses/cse190m/11sp/homework/2/rottenbig.png" alt="Rotten">
+                    <img src="https://www.cs.washington.edu/education/courses/cse190m/11sp/homework/2/rottenbig.png" alt="Rotten">
                     <?php
                 }
             ?>
@@ -109,37 +98,43 @@
 		<div class="review-cols">
 
             <?php
-            for($i=0; $i < count($review)/2 ; $i++){ //here i print half the review in the first column
+            for($i=0; $i < count($review)/2 ; $i++){ //here I print half the review in the first column
                 ?>
 
                 <div class="review">
                     <p class="review_box">
-                        <img src="http://www.cs.washington.edu/education/courses/cse190m/11sp/homework/2/<?=str_replace(" ", "", strtolower($review[$i][1])) ?>.gif" alt="Rotten">
+                        <img src="https://www.cs.washington.edu/education/courses/cse190m/11sp/homework/2/<?=str_replace(" ", "", strtolower($review[$i][1])) ?>.gif" alt="Rotten">
                         <q><?= $review[$i][0] ?></q>
                     </p>
-                    <p class="review_name">
-                        <img src="http://www.cs.washington.edu/education/courses/cse190m/11sp/homework/2/critic.gif" alt="Critic">
+                    <p class='review_name'>
+                        <img src="https://www.cs.washington.edu/education/courses/cse190m/11sp/homework/2/critic.gif" alt="Critic">
                         <?= $review[$i][2] ?> <br>
-                    <div class="editor"><?= $review[$i][3] ?></div>
+                        <span class="editor"><?= $review[$i][3] ?></span>
                     </p>
                 </div>
 
             <?php
             }
-            echo "</div> <div class='review-cols'>";
-            for($i=(count($review) / 2)+1; $i < count($review) ; $i++){ //here i print the second half of the review in the second column
+            ?>
+        </div>
+        <div class='review-cols'>
+            <?php
+            for($i=(count($review) / 2)+1; $i < count($review) ; $i++){ //here I print the second half of the review in the second column
                 ?>
 
                 <div class="review">
                     <p class="review_box">
-                        <img src="http://www.cs.washington.edu/education/courses/cse190m/11sp/homework/2/<?=str_replace(" ", "", strtolower($review[$i][1])) ?>.gif" alt="Rotten">
+                        <img src="https://www.cs.washington.edu/education/courses/cse190m/11sp/homework/2/<?=str_replace(" ", "", strtolower($review[$i][1])) ?>.gif" alt="Rotten">
                         <q><?= $review[$i][0] ?></q>
                     </p>
                     <p class="review_name">
-                        <img src="http://www.cs.washington.edu/education/courses/cse190m/11sp/homework/2/critic.gif" alt="Critic">
-                        <?= $review[$i][2] ?> <br>
-                    <div class="editor"><?= $review[$i][3] ?></div>
-                    </p>
+                        <img src="https://www.cs.washington.edu/education/courses/cse190m/11sp/homework/2/critic.gif" alt="Critic" />
+                        <?= $review[$i][2] ?>
+                        <br>
+                    <span class="editor">
+                        <?= $review[$i][3] ?>
+                    </span>
+                </p>
                 </div>
 
             <?php
@@ -155,9 +150,11 @@
 
 <div class="validation">
 	<p>
-		<a href="http://validator.w3.org/check/referer"><img width="88" src="https://upload.wikimedia.org/wikipedia/commons/b/bb/W3C_HTML5_certified.png" alt="Valid HTML5!"></a>
-	<p> <br>
-		<a href="http://jigsaw.w3.org/css-validator/check/referer"><img img src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!"></a>
+		<a href="https://validator.w3.org/check/referer"><img width="88" src="https://upload.wikimedia.org/wikipedia/commons/b/bb/W3C_HTML5_certified.png" alt="Valid HTML5!"></a>
+	</p> <br>
+    <p>
+		<a href="https://jigsaw.w3.org/css-validator/check/referer"><img src="https://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!"></a>
+    </p>
 </div>
 </body>
 </html>
