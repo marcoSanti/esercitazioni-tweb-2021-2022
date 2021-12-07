67392c1d5a019f8c84f6cc430426aaeab2edc4d4
marco santimaria <marco.santimaria@edu.unito.it>
2021-12-07 19:33:29 +0100

    began work on course project

---
+++ Progetto finale/uniShare/search.php
@@ -0,0 +1,55 @@
+<?php
+include "header.html";
+include "navbar.php";
+?>
+
+<div class="Container" id="SearchPageContainer">
+    <div class="row">
+        <!--filter options -->
+        <div class="col" id="SearchPageFilterOptions">
+            <form class="row g-3">
+                <div class="row-md-1">
+                    <label for="inputState" class="form-label">State</label>
+                    <select id="inputState" class="form-select">
+                        <option selected>Choose...</option>
+                        <option>...</option>
+                    </select>
+                </div>
+                <div class="row-md-1">
+                    <label for="inputState" class="form-label">State</label>
+                    <select id="inputState" class="form-select">
+                        <option selected>Choose...</option>
+                        <option>...</option>
+                    </select>
+                </div>
+                <div class="row-md-1">
+                    <label for="inputState" class="form-label">State</label>
+                    <select id="inputState" class="form-select">
+                        <option selected>Choose...</option>
+                        <option>...</option>
+                    </select>
+                </div>
+                <div class="row-md-1">
+                    <label for="inputState" class="form-label">State</label>
+                    <select id="inputState" class="form-select">
+                        <option selected>Choose...</option>
+                        <option>...</option>
+                    </select>
+                </div>
+                <div class="col-12">
+                    <button type="submit" class="btn btn-primary">Sign in</button>
+                </div>
+            </form>
+        </div>
+
+
+        <!-- main page -->
+        <div class="col">
+            asasas
+        </div>
+    </div>
+</div>
+
+<?php
+include "footer.html";
+?>
